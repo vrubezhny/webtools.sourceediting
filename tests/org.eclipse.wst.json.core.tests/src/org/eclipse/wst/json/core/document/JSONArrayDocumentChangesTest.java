ba466284193b65a73844fbe08dacb7c35a86d303
Victor Rubezhny <vrubezhny@exadel.com>
2016-02-24 17:26:40 +0300

    Bug 486238 - Add JUnit Tests for JSON Editor
    JSON Core tests are added
    
    Signed-off-by: Victor Rubezhny <vrubezhny@exadel.com>

---
+++ tests/org.eclipse.wst.json.core.tests/src/org/eclipse/wst/json/core/document/JSONArrayDocumentChangesTest.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
+package org.eclipse.wst.json.core.document;
+
+import org.eclipse.wst.json.core.TestUtil;
+import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JSONArrayDocumentChangesTest {
+
+	@Test
+	public void testInsertCloseArray() {
+		IJSONModel model = TestUtil.loadModel("[");
+		IJSONDocument document = model.getDocument();
+
+		// Document contains an Array which is not closed
+		Assert.assertNotNull(document.getFirstChild());
+		Assert.assertTrue(document.getFirstChild() instanceof IJSONArray);
+		IJSONArray array = (IJSONArray) document.getFirstChild();
+		Assert.assertFalse(array.isClosed());
+
+		IStructuredDocument structuredDocument = model.getStructuredDocument();
+		// insert at end ']'
+		structuredDocument.replaceText(structuredDocument, 1, 0, "]");
+		Assert.assertTrue(array.isClosed());
+	}
+}
